8576d4bb6075b35b366505eba3f782c1c560acde
Kaviraj Gosaye <45905404+kaviraj07@users.noreply.github.com>
2025-04-08 22:28:36 +0000

    api_root fix

--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -7,19 +7,19 @@ from .models import User, Team, Activity, Leaderboard, Workout
 
 @api_view(['GET'])
 def api_root(request, format=None):
-    # Dynamically determine the base URL
-    base_url = request.build_absolute_uri('/')
-    if 'REPLACE-THIS-WITH-YOUR-CODESPACE-NAME' in base_url:
-        base_url = 'https://jubilant-broccoli-4wwgr6r55g537574-8000.app.github.dev/'
-    else:
-        base_url = 'http://127.0.0.1:8000/'
+    # # Dynamically determine the base URL
+    # base_url = request.build_absolute_uri('/')
+    # if 'REPLACE-THIS-WITH-YOUR-CODESPACE-NAME' in base_url:
+    #     base_url = 'https://jubilant-broccoli-4wwgr6r55g537574-8000.app.github.dev/'
+    # else:
+    #     base_url = 'http://127.0.0.1:8000/'
 
     return Response({
-        'users': base_url + 'api/users/',
-        'teams': base_url + 'api/teams/',
-        'activities': base_url + 'api/activities/',
-        'leaderboard': base_url + 'api/leaderboard/',
-        'workouts': base_url + 'api/workouts/'
+        'users':request.build_absolute_uri('api/users/'),
+        'teams':request.build_absolute_uri('api/teams/'),
+        'activities':request.build_absolute_uri('api/activities/'),
+        'leaderboard':request.build_absolute_uri('api/leaderboard/'),
+        'workouts':request.build_absolute_uri('api/workouts/')
     })
 
 class UserViewSet(viewsets.ModelViewSet):
